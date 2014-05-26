dd9e2cacf593a7875f6aeba6df39aa22e209cd10
Brandon <brandon@bremaweb.com>
2014-05-25 23:14:14 -0500

    Start of chat mod

---
+++ mods/chat/init.lua
@@ -0,0 +1,19 @@
+chat = {}
+
+--[[
+minetest.register_on_chat_message(function(name,msg)
+
+end) 
+]]
+
+chat.local_chat = function(pos,text,radius)
+  if radius == nil then
+    radius = 25
+  end
+  local oir = minetest.get_objects_inside_radius(pos, radius)
+  for _,p in pairs(oir) do
+    if p:is_player() then
+      minetest.chat_send_player(p:get_player_name(),text)
+    end
+  end
+end
