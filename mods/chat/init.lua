fe5336ce6630c2ab2a19e56889944e9a6a2db5d6
Brandon <brandon@bremaweb.com>
2014-07-07 21:56:56 -0500

    start of limited range chat feature

--- mods/chat/init.lua
+++ mods/chat/init.lua
@@ -7,13 +7,15 @@ end)
 ]]
 
 chat.local_chat = function(pos,text,radius)
-  if radius == nil then
-    radius = 25
-  end
-  local oir = minetest.get_objects_inside_radius(pos, radius)
-  for _,p in pairs(oir) do
-    if p:is_player() then
-      minetest.chat_send_player(p:get_player_name(),text)
-    end
-  end
+	if radius == nil then
+		radius = 25
+	end
+	if pos ~= nil then
+		local oir = minetest.get_objects_inside_radius(pos, radius)
+		for _,p in pairs(oir) do
+			if p:is_player() then
+				minetest.chat_send_player(p:get_player_name(),text)
+			end
+		end
+	end
 end
